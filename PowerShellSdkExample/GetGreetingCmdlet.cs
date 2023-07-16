7345dfd1e363e6fd838d6952a828fcd854928c58
Sebastian Kämpfer <kontakt@skaempfer.com>
2023-07-16 15:58:35 +0200

    Add example PowerShell module project for testing Sdk

---
+++ PowerShellSdkExample/GetGreetingCmdlet.cs
@@ -0,0 +1,20 @@
+﻿using System.Management.Automation;
+
+namespace PowerShellSdkExample;
+
+[Cmdlet(VerbsCommon.Get, "GreetingCmdlet")]
+[OutputType(typeof(string))]
+public class GetGreetingCmdlet : PSCmdlet
+{
+  [Parameter(
+    Mandatory = true,
+    Position = 0,
+    ValueFromPipeline = true,
+    ValueFromPipelineByPropertyName = true)]
+  public string Name { get; set; }
+
+  protected override void ProcessRecord()
+  {
+    WriteObject($"Hello {this.Name}!");
+  }
+}
